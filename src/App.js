74f8bb15d90318e12ec052dd5a4ad758fad89fdc
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-17 15:53:36 +0530

    Prettified and added profile route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { Navbar } from './components/layout'
+import { SignUp } from './components/auth'
 
 export default () => {
-    return (
-        <Router>
-            <Navbar/>
-            <Switch>
-                <Route path="/signin" component={() => <h1>Sign In</h1>} />
-                <Route path="/signup" component={() => <h1>Sign Up</h1>} />
-                <Route path="/" component={() => <h1>Home</h1>} />
-            </Switch>
-        </Router>
-    )
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path='/signin' component={() => <h1>Sign In</h1>} />
+				<Route path='/signup' component={() => <SignUp />} />
+				<Route path='/profile' component={() => <h1>Profile</h1>} />
+				<Route path='/' component={() => <h1>Home</h1>} />
+			</Switch>
+		</Router>
+	)
 }
