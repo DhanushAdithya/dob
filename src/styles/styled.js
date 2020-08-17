134af1bfbb44df953d12626a7a5cf91454775d49
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-17 12:51:39 +0530

    Separated styled-components file to styles folder

---
+++ src/styles/styled.js
@@ -0,0 +1,16 @@
+import { Button, Avatar } from '@material-ui/core'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledNavButton = styled(Button)`
+    margin: 0 0.6rem;
+`
+const StyledNavAvatar = styled(Avatar)`
+    margin: 0 0 0 0.6rem;
+    background-color: #323330ee;
+`
+const StyledNavLink = styled(NavLink)`
+    text-decoration: none;
+`
+
+export { StyledNavButton, StyledNavAvatar, StyledNavLink }
