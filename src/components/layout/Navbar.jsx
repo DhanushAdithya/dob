48d171118b8fe796ad5219ab40e7a68dbdda56b6
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-17 12:50:45 +0530

    Added the NavLink components to the Navbar.jsx

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { SignedIn, SignedOut } from '../layout'
+
+const useStyles = makeStyles((theme) => ({
+    color: {
+        backgroundColor: '#F0DB4F',
+        color: '#323330'
+    },
+    logo: {
+        marginRight: 'auto'
+    }
+}))
+
+export default () => {
+    const classes = useStyles()
+
+    return (
+        <AppBar className={classes.color} position="static">
+            <Toolbar>
+                <Typography variant="h5" className={classes.logo}>Hello There</Typography>
+                <SignedOut />
+                <SignedIn />
+            </Toolbar>
+        </AppBar>
+    )
+}
