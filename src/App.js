b3949ccad2c8fb5ce86a943491449a1f8b35fff6
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-18 17:02:33 +0530

    Added SignIn Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { Navbar } from './components/layout'
-import { SignUp } from './components/auth'
+import { SignUp, SignIn } from './components/auth'
 
 export default () => {
 	return (
 		<Router>
 			<Navbar />
 			<Switch>
-				<Route path='/signin' component={() => <h1>Sign In</h1>} />
+				<Route path='/signin' component={() => <SignIn />} />
 				<Route path='/signup' component={() => <SignUp />} />
 				<Route path='/profile' component={() => <h1>Profile</h1>} />
 				<Route path='/' component={() => <h1>Home</h1>} />
