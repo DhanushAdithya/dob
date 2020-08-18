d2c35d75ec36b46e1d37b2c63dbee0268172b736
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-18 19:50:31 +0530

    Added state for confirming password

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, Box } from '@material-ui/core'
 import CheckCircleIcon from '@material-ui/icons/CheckCircle'
 
@@ -10,6 +10,12 @@ import {
 } from '../../styles/styled'
 
 export default () => {
+	const [pass, setPass] = useState({
+		pass: '',
+		confirmPass: '',
+		valid: false,
+	})
+
 	return (
 		<StyledFormContainer maxWidth='sm'>
 			<StyledFormPaper component='form'>
@@ -17,22 +23,54 @@ export default () => {
 					Sign Up
 				</Typography>
 				<StyledTextField label='Name' fullWidth />
-				<StyledTextField label='Email' fullWidth />
-				<StyledTextField label='Password' fullWidth />
+				<StyledTextField label='Email' type='email' fullWidth />
+				<StyledTextField
+					label='Password'
+					type='password'
+					value={pass.pass}
+					onChange={e => {
+						setPass({
+							...pass,
+							pass: e.target.value,
+							valid: e.target.value === pass.confirmPass,
+						})
+					}}
+					fullWidth
+				/>
 				<Box display='flex'>
-					<StyledTextField label='Confirm Password' fullWidth />
+					<StyledTextField
+						label='Confirm Password'
+						type='password'
+						value={pass.confirmPass}
+						onChange={e => {
+							setPass({
+								...pass,
+								confirmPass: e.target.value,
+								valid: e.target.value === pass.pass,
+							})
+						}}
+						fullWidth
+					/>
 					<CheckCircleIcon
-						color='primary'
+						color={
+							pass.valid && pass.confirmPass !== '' ? 'primary' : 'error'
+						}
 						fontSize='large'
 						style={{
 							margin: 'auto 0',
 							borderRadius: '30px',
 							backgroundColor: '#ddda',
 							padding: '5px',
+							transition: '0.3s',
 						}}
 					/>
 				</Box>
-				<StyledSubmit type='submit' color='secondary' variant='outlined'>
+				<StyledSubmit
+					type='submit'
+					color='secondary'
+					variant='outlined'
+					disabled={!pass.valid}
+				>
 					Submit
 				</StyledSubmit>
 			</StyledFormPaper>
