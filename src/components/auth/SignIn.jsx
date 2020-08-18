d7b919d7b499eaf6edacd73d3a86db72474c1d39
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-18 17:03:15 +0530

    Modified SignIn Component

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
+
+import {
+	StyledTextField,
+	StyledSubmit,
+	StyledFormContainer,
+	StyledFormPaper,
+} from '../../styles/styled'
 
 export default () => {
-	return <div />
+	return (
+		<StyledFormContainer maxWidth='sm'>
+			<StyledFormPaper component='form'>
+				<Typography variant='h4' align='center'>
+					Sign In
+				</Typography>
+				<StyledTextField label='Email' fullWidth />
+				<StyledTextField label='Password' fullWidth />
+				<StyledSubmit type='submit' color='secondary' variant='outlined'>
+					Login
+				</StyledSubmit>
+			</StyledFormPaper>
+		</StyledFormContainer>
+	)
 }
