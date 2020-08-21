528696cdfb44bb3156f121ac65d83d8ba00b91cd
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-21 15:13:02 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { render } from 'react-dom'
 import App from './App'
 import './styles/style.scss'
 import * as serviceWorker from './serviceWorker'
-// import { createStore } from 'redux'
-// import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 const root = document.getElementById('root')
 
