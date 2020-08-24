b309b3f927f4ea3d1608ba572f21e7a0858e90ee
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-24 13:30:40 +0530

    Applied thunk middleware to redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import App from './App'
 import './styles/style.scss'
 import { rootReducer } from './store/reducers'
 import * as serviceWorker from './serviceWorker'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 const root = document.getElementById('root')
 
 render(
