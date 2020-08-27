0910e5cba622b8ae8e2ec40cb1f267c334254d6e
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-27 20:57:54 +0530

    Added styled to list items in Month.jsx

--- src/components/months/Month.jsx
+++ src/components/months/Month.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledList = styled.li`
+    list-style-type: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 3px;
+    background-color: #f0db4f;
+    margin: 2px;
+`
 
 export default () => {
 	return (
-		<ul>
+		<ul style={{ display: 'flex' }}>
 			{Array.from({ length: 31 }, (v, i) => i + 1).map(e => {
-				return <li key={'J' + e}>g</li>
+				return <StyledList key={'J' + e}></StyledList>
 			})}
 		</ul>
 	)
