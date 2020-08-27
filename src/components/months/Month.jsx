7dd511dc5ba6e5de6c7c32b382c6c9d5729e6121
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-27 21:03:13 +0530

    Modified some CSS stuffs

--- src/components/months/Month.jsx
+++ src/components/months/Month.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledList = styled.li`
-    list-style-type: none;
-    width: 20px;
-    height: 20px;
-    border-radius: 3px;
-    background-color: #f0db4f;
-    margin: 2px;
+	list-style-type: none;
+	width: 30px;
+	height: 30px;
+	border-radius: 3px;
+	background-color: #f0db4f;
+	margin: 2px;
 `
 
 export default () => {
 	return (
-		<ul style={{ display: 'flex' }}>
+		<ul style={{ display: 'flex', flexWrap: 'wrap' }}>
 			{Array.from({ length: 31 }, (v, i) => i + 1).map(e => {
 				return <StyledList key={'J' + e}></StyledList>
 			})}
