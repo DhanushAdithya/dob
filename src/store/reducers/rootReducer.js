7765ba59a8b926d816f8ad5bb756a1412749472e
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-28 16:12:46 +0530

    Combined monthReducer to the rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,9 @@
-import { authReducer } from '../reducers'
+import { authReducer, monthReducer } from '../reducers'
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
 	auth: authReducer,
+	month: monthReducer,
 })
 
 export default rootReducer
