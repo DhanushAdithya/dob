0632090ff805022a93b5c02396d9f6dec6568c6d
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-29 16:09:42 +0530

    Added new shades

--- src/assets/shades.js
+++ src/assets/shades.js
@@ -1,1 +1,3 @@
-export const SHADES = ['#F0DB4F', '#F2E065', '#F4E47B', '#F6E991', '#F8EDA7']
+export const SHADES = ['#f5c88e', '#f5ba6e', '#f7ac4a', '#fa9b20', '#f78c02']
+// ['#FCE653','#F0DB4F','#D6C347','#B0A03A','#706625']
+// ['#F8EDA7', '#F6E991', '#F4E47B', '#F2E065', '#F0DB4F']
