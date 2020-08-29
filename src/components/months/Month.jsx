754c70a458eb0e2c365a86da38da691d40e8e620
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-29 16:10:09 +0530

    Moar styled! and redux connect

--- src/components/months/Month.jsx
+++ src/components/months/Month.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
+import { Tooltip } from '@material-ui/core'
+import { connect } from 'react-redux'
+
+import { SHADES } from '../../assets/shades'
 
 const StyledList = styled.li`
-	list-style-type: none;
-	width: 30px;
-	height: 30px;
 	border-radius: 3px;
-	background-color: #f0db4f;
-	margin: 2px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: ${({ prop }) => (prop ? prop : '#f8eda7')};
 `
 
-export default () => {
+const StyledUL = styled.li`
+	display: grid;
+	grid-template-columns: repeat(7, 25px);
+	grid-template-rows: repeat(5, 25px);
+	gap: 5px;
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	margin-top: 30px;
+	margin-left: 30px;
+`
+
+const Month = ({ month }) => {
+	console.log(month)
 	return (
-		<ul style={{ display: 'flex', flexWrap: 'wrap' }}>
-			{Array.from({ length: 31 }, (v, i) => i + 1).map(e => {
-				return <StyledList key={'J' + e}></StyledList>
-			})}
-		</ul>
+		<StyledUL>
+			{month.dates.map(e => (
+				<Fragment key={`J${e}`}>
+					<Tooltip title={`${e} DOBs`} placement='top' arrow>
+						<StyledList prop={SHADES[+e]}></StyledList>
+					</Tooltip>
+				</Fragment>
+			))}
+		</StyledUL>
 	)
 }
+
+const mapStateToProps = state => ({
+	month: state.month,
+})
+
+const mapDispatchToProps = null
+
+export default connect(mapStateToProps, mapDispatchToProps)(Month)
