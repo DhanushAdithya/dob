f882f13a18a4eb0dad77cb0c62a20a350695d58a
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-30 22:25:02 +0530

    Added MonthContainer component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { Navbar } from './components/layout'
 import { SignUp, SignIn } from './components/auth'
-import { Create, Month } from './components/months'
+import { Create, Month, MonthContainer } from './components/months'
 
 export default () => {
 	return (
 		<Router>
 			<Navbar />
 			<Switch>
-				<Route path='/signin' component={() => <SignIn />} />{' '}
-				<Route path='/signup' component={() => <SignUp />} />{' '}
+				<Route path='/signin' component={() => <SignIn />} />
+				<Route path='/signup' component={() => <SignUp />} />
 				<Route path='/profile' component={() => <h1> Profile </h1>} />
-				<Route
-					path='/jan'
-					component={() => (
-						<>
-							<Month />
-							<Month />
-							<Month />
-						</>
-					)}
-				/>{' '}
-				<Route path='/' component={() => <Create />} />{' '}
-			</Switch>{' '}
+				<Route path='/jan' component={() => <MonthContainer />} />
+				<Route path='/' component={() => <Create />} />
+			</Switch>
 		</Router>
 	)
 }
