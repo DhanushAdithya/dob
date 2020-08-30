e7332b83a29bd96571a5786acc3efed390fe07a6
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-30 16:06:33 +0530

    Imported and used the firebase packages with thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { getFirestore, reduxFirestore } from 'redux-firestore'
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase'
 
 import App from './App'
 import './styles/style.scss'
 import { rootReducer } from './store/reducers'
+import firebase from './config/firebase'
 import * as serviceWorker from './serviceWorker'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(
+	rootReducer,
+	compose(
+		applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+		reduxFirestore(firebase),
+		reactReduxFirebase(firebase)
+	)
+)
 const root = document.getElementById('root')
 
 render(
