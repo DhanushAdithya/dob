3e90a596f37562aa6e4074045557c445dc8306da
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-30 20:03:09 +0530

    Modified and got the result I was expecting

--- src/store/actions/addDate.js
+++ src/store/actions/addDate.js
@@ -1,9 +1,21 @@
-export const addDate = date => {
+export const addDate = ({ month, date }) => {
 	return (dispatch, getState, { getFirebase, getFirestore }) => {
-        const firestore = getFirestore()
+		const firestore = getFirestore()
 		firestore
 			.collection('months')
-			// .add()
-			// .then(() => dispatch({ type: 'ADD_DATE', date }))
+			.doc(month)
+			.get()
+			.then(doc => {
+				let datesArray = doc.data().dates
+				datesArray[+date - 1] = datesArray[+date - 1] + 1
+				return datesArray
+			})
+			.then(data => {
+				firestore.collection('months').doc(month).set({
+					month: month,
+					dates: data,
+				})
+			})
+			.then(() => dispatch({ type: 'ADD_DATE', date }))
 	}
 }
