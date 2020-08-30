926836f7b3a50443bc203d2ccdb76bb8d49d1ac1
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-30 16:07:49 +0530

    Added the test action to the monthReducer

--- src/store/reducers/monthReducer.js
+++ src/store/reducers/monthReducer.js
@@ -11,5 +11,11 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-	return state
+	switch (action.type) {
+		case 'ADD_DATE':
+			console.log('Succesfully added the date!')
+			return state
+		default:
+			return state
+	}
 }
