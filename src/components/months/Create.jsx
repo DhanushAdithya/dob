20412f15f475257590e23ed4c2e1d96fd6a9cadd
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-30 16:07:07 +0530

    Removed MONTHS constant

--- src/components/months/Create.jsx
+++ src/components/months/Create.jsx
@@ -14,29 +14,16 @@ import {
 	StyledSelect,
 } from '../../styles/styled'
 import { Formtheme } from '../../styles/muiTheme'
+import { MONTHS } from '../../assets/months'
 
 export default () => {
 	const [month, setMonth] = useState('April')
 	const [date, setDate] = useState('1')
-	const MONTHS = [
-		{ month: 'January', days: 31 },
-		{ month: 'February', days: 29 },
-		{ month: 'March', days: 31 },
-		{ month: 'April', days: 30 },
-		{ month: 'May', days: 31 },
-		{ month: 'June', days: 30 },
-		{ month: 'July', days: 31 },
-		{ month: 'August', days: 31 },
-		{ month: 'September', days: 30 },
-		{ month: 'October', days: 31 },
-		{ month: 'November', days: 30 },
-		{ month: 'December', days: 31 },
-	]
 
 	return (
 		<ThemeProvider theme={Formtheme}>
 			<StyledFormContainer maxWidth='sm'>
-				<StyledFormPaper component='form'>
+				<StyledFormPaper component='form' onSubmit={() => {}}>
 					<Typography variant='h4' align='center'>
 						Create
 					</Typography>
