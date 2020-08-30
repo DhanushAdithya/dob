b7559c7ae7a0981f7fe75b200327d41aa49ceaa4
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-30 16:35:51 +0530

    Moved shades and months to single index.js

---
+++ src/assets/index.js
@@ -0,0 +1,18 @@
+export const MONTHS = [
+	{ month: 'January', days: 31 },
+	{ month: 'February', days: 29 },
+	{ month: 'March', days: 31 },
+	{ month: 'April', days: 30 },
+	{ month: 'May', days: 31 },
+	{ month: 'June', days: 30 },
+	{ month: 'July', days: 31 },
+	{ month: 'August', days: 31 },
+	{ month: 'September', days: 30 },
+	{ month: 'October', days: 31 },
+	{ month: 'November', days: 30 },
+	{ month: 'December', days: 31 },
+]
+
+export const SHADES = ['#f5c88e', '#f5ba6e', '#f7ac4a', '#fa9b20', '#f78c02']
+// ['#FCE653','#F0DB4F','#D6C347','#B0A03A','#706625']
+// ['#F8EDA7', '#F6E991', '#F4E47B', '#F2E065', '#F0DB4F']
