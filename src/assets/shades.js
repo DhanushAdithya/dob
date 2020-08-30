65a06c5df5f73bfdf3725b9ec686f01d19dfec57
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-30 16:35:28 +0530

    Deleted months.js and shades.js

--- src/assets/shades.js
+++
@@ -1,3 +0,0 @@
-export const SHADES = ['#f5c88e', '#f5ba6e', '#f7ac4a', '#fa9b20', '#f78c02']
-// ['#FCE653','#F0DB4F','#D6C347','#B0A03A','#706625']
-// ['#F8EDA7', '#F6E991', '#F4E47B', '#F2E065', '#F0DB4F']
