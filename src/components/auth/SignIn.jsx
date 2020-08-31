cac2a92df181b03faa73ec8cdf8791ee242be41d
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-31 21:51:26 +0530

    Added state and redux action for signIn

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/core/styles'
+import { connect } from 'react-redux'
 
 import {
 	StyledTextField,
@@ -9,17 +10,41 @@ import {
 	StyledFormPaper,
 } from '../../styles/styled'
 import { Formtheme } from '../../styles/muiTheme'
+import { signIn } from '../../store/actions/authUser'
+import { useState } from 'react'
 
-export default () => {
+const SignIn = ({ signIn }) => {
+	const [user, setUser] = useState({ email: '', password: '' })
+	const updateUser = e => setUser({ ...user, [e.target.id]: e.target.value })
+
 	return (
 		<ThemeProvider theme={Formtheme}>
 			<StyledFormContainer maxWidth='sm'>
-				<StyledFormPaper component='form'>
+				<StyledFormPaper
+					component='form'
+					onSubmit={e => {
+						e.preventDefault()
+						signIn(user)
+					}}
+				>
 					<Typography variant='h4' align='center'>
 						Sign In
 					</Typography>
-					<StyledTextField label='Email' fullWidth />
-					<StyledTextField label='Password' fullWidth />
+					<StyledTextField
+						label='Email'
+						id='email'
+						onChange={updateUser}
+						value={user.email}
+						fullWidth
+					/>
+					<StyledTextField
+						label='Password'
+						id='password'
+						onChange={updateUser}
+						value={user.password}
+						type='password'
+						fullWidth
+					/>
 					<StyledSubmit color='primary' variant='outlined'>
 						Login
 					</StyledSubmit>
@@ -28,3 +53,13 @@ export default () => {
 		</ThemeProvider>
 	)
 }
+
+const mapStateToProps = state => {
+    console.log(state)
+}
+
+const mapDispatchToProps = dispatch => ({
+	signIn: user => dispatch(signIn(user)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
