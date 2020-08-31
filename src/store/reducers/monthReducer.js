f65ead30eea54c1a27bc525721788f72a4145404
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-08-31 21:55:18 +0530

    Removed initial state (dummy data)

--- src/store/reducers/monthReducer.js
+++ src/store/reducers/monthReducer.js
@@ -1,14 +1,4 @@
-const initialState = {
-	month: 'January',
-	// prettier-ignore
-	dates: [
-        0, 1, 2, 3, 4, 3, 2,
-        1, 2, 3, 4, 2, 2, 1,
-        3, 3, 2, 4, 1, 0, 2,
-        3, 4, 2, 0, 1, 4, 2,
-        0, 1, 1,
-    ]
-}
+const initialState = {}
 
 export default (state = initialState, action) => {
 	switch (action.type) {
