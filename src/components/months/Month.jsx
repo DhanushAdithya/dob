2d3dcb6a185c9e5176ff0764a1d733dba32b4780
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-01 20:55:43 +0530

    Added styles with styled-components to month

--- src/components/months/Month.jsx
+++ src/components/months/Month.jsx
@@ -1,25 +1,35 @@
 import React, { Fragment } from 'react'
-import { Tooltip } from '@material-ui/core'
+import { Tooltip, Typography } from '@material-ui/core'
 
-import { StyledList, StyledUL } from '../../styles/styled'
+import { StyledMonthWrapper, StyledList, StyledUL } from '../../styles/styled'
 import { SHADES } from '../../assets'
+
 
 export default ({ month, dates }) => {
 	return (
-		<StyledUL>
-			{dates.map((e, _) => (
-				<Fragment key={`J${_}`}>
-					<Tooltip
-						title={`${e === 0 ? 'No' : e} DOB${e !== 1 ? 's' : ''} on ${
-							month.slice(0, 3) + ', ' + (+_ + 1)
-						}`}
-						placement='top'
-						arrow
-					>
-						<StyledList shade={SHADES[+e]}></StyledList>
-					</Tooltip>
-				</Fragment>
-			))}
-		</StyledUL>
+		<StyledMonthWrapper>
+			<Typography
+				style={{ color: '#f0db4f' }}
+				variant='h5'
+				align='center'
+			>
+				{month}
+			</Typography>
+			<StyledUL>
+				{dates.map((e, _) => (
+					<Fragment key={`J${_}`}>
+						<Tooltip
+							title={`${e === 0 ? 'No' : e} DOB${
+								e !== 1 ? 's' : ''
+							} on ${month.slice(0, 3) + ', ' + (+_ + 1)}`}
+							placement='top'
+							arrow
+						>
+							<StyledList shade={SHADES[+e]}></StyledList>
+						</Tooltip>
+					</Fragment>
+				))}
+			</StyledUL>
+		</StyledMonthWrapper>
 	)
 }
