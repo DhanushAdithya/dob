9529155b121ce6d00368f018295ac3d64d4ddbe8
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-01 16:11:30 +0530

    Added firestore users collection for user signUps

--- src/store/actions/authUser.js
+++ src/store/actions/authUser.js
@@ -8,22 +8,36 @@ export const signIn = ({ email, password }) => {
 	}
 }
 
-export const signUp = ({ email, password }) => {
-	return (dispatch, getState, { getFirebase }) => {
+export const signUp = ({ name, email, password }) => {
+	return (dispatch, getState, { getFirebase, getFirestore }) => {
 		const firebase = getFirebase()
 		firebase
 			.auth()
 			.createUserWithEmailAndPassword(email, password)
+			.then(({ user }) => {
+				const firestore = getFirestore()
+				firestore
+					.collection('users')
+					.doc(user.uid)
+					.set({
+						name,
+						email,
+						initials: name
+							.split(' ')
+							.map(_ => _[0])
+							.join(''),
+					})
+			})
 			.then(() => dispatch({ type: 'SIGN_UP' }))
 	}
 }
 
 export const signOut = () => {
-    return (dispatch, getState, { getFirebase }) => {
-        const firebase = getFirebase()
-        firebase
-            .auth()
-            .signOut()
-            .then(() => dispatch({ type: 'SIGN_OUT' }))
-    }
+	return (dispatch, getState, { getFirebase }) => {
+		const firebase = getFirebase()
+		firebase
+			.auth()
+			.signOut()
+			.then(() => dispatch({ type: 'SIGN_OUT' }))
+	}
 }
