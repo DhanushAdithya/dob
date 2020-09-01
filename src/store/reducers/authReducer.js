0d34d739199988dab8f24accaae234db18661e19
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-01 16:11:51 +0530

    Route redirecting returns in Reducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,13 +1,13 @@
-const initialState = {}
+const initialState = { redirect: false }
 
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case 'SIGN_IN':
 			console.log('Succesfully signed in!')
-			return state
+			return { ...state, redirect: true }
 		case 'SIGN_UP':
 			console.log('Succesfully signed up!')
-			return state
+			return { ...state, redirect: true }
 		case 'SIGN_OUT':
 			console.log('Succesfully signed out!')
 			return state
