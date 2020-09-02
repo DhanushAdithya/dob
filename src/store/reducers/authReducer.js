1ee7be7d31ece51cd104c17e327d804a6a53ac1f
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-02 20:10:08 +0530

    Set error msg to empty after successful operations

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -3,14 +3,11 @@ const initialState = { redirect: false, err: '' }
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case 'SIGN_IN':
-			console.log('Succesfully signed in!')
-			return { ...state, redirect: true }
+			return { err: '', redirect: true }
 		case 'SIGN_UP':
-			console.log('Succesfully signed up!')
-			return { ...state, redirect: true }
+			return { err: '', redirect: true }
 		case 'SIGN_OUT':
-			console.log('Succesfully signed out!')
-			return state
+			return { err: '', redirect: false }
 		case 'SIGN_IN_ERROR':
 			return { ...state, err: action.err }
 		case 'SIGN_UP_ERROR':
