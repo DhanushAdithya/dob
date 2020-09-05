c3aacc50678f1074ed4003e74e012397964fdb35
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-05 21:09:30 +0530

    Dummy content for Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default () => {
 	return (
 		<div>
 			<h1 children='Home' />
+			<p>Go to the month chart</p>
+			<Link to='/months' children='GO!!' />
 		</div>
 	)
 }
