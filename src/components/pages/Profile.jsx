854eb8a58eff2cf70cbc2a843e0f5c2a20e624ff
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-06 13:31:20 +0530

    Added details to the Profile component

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/core/styles'
+import { connect } from 'react-redux'
 
 import { StyledContainer, StyledPaper } from '../../styles/styled'
 import { Formtheme } from '../../styles/muiTheme'
+import { Details } from '../reusable'
 
-export default () => {
+const Profile = ({ user }) => {
 	return (
 		<ThemeProvider theme={Formtheme}>
 			<StyledContainer maxWidth='sm'>
 				<StyledPaper>
 					<Typography variant='h4' align='center' children='Profile' />
+					<Details name='Name' value={user.name} />
+					<Details name='Email' value={user.email} />
+					<Details name='Initials' value={user.initials} />
+					<Details name='ID' value={user.id} />
+					<Details
+						name='Can you add a date?'
+						value={
+							!user.isAdded
+								? 'Ofcourse you can!'
+								: 'It seems to be you already added a date, so no unfourtunately'
+						}
+					/>
 				</StyledPaper>
 			</StyledContainer>
 		</ThemeProvider>
 	)
 }
+
+const mapStateToProps = state => ({
+	user: state.firebase.profile,
+})
+
+const mapDispatchToProps = null
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
