1b3eee32a562a38551bc0a0f37bb9f1955105271
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-07 19:27:14 +0530

    Edited the env variable names

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-	apiKey: process.env.FB_API_KEY,
-	authDomain: `${process.env.FB_ID}.firebaseapp.com`,
-	databaseURL: `https://${process.env.FB_ID}.firebaseio.com`,
-	projectId: process.env.FB_ID,
-	storageBucket: `${process.env.FB_ID}.appspot.com`,
-	messagingSenderId: process.env.FB_MSG_ID,
-	appId: process.env.FB_APP_ID,
-	measurementId: process.env.FB_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_FB_API_KEY,
+	authDomain: `${process.env.REACT_APP_FB_ID}.firebaseapp.com`,
+	databaseURL: `https://${process.env.REACT_APP_FB_ID}.firebaseio.com`,
+	projectId: process.env.REACT_APP_FB_ID,
+	storageBucket: `${process.env.REACT_APP_FB_ID}.appspot.com`,
+	messagingSenderId: process.env.REACT_APP_FB_MSG_ID,
+	appId: process.env.REACT_APP_FB_APP_ID,
+	measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
 }
 
 firebase.initializeApp(config)
