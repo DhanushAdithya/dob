5f605a2244e965106fe40a344befc5ffa37c5842
Dhanush Adithya <s.dhanushadithya@gmail.com>
2020-09-07 18:31:33 +0530

    Added the projectID to the config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,10 +5,10 @@ import 'firebase/auth'
 
 const config = {
 	apiKey: process.env.FB_API_KEY,
-	authDomain: `${process.env.FB_ID}.firebaseapp.com`,
-	databaseURL: `https://${process.env.FB_ID}.firebaseio.com`,
-	projectId: process.env.FB_ID,
-	storageBucket: `${process.env.FB_ID}.appspot.com`,
+	authDomain: `dobg-26dd9.firebaseapp.com`,
+	databaseURL: `https://dobg-26dd9.firebaseio.com`,
+	projectId: 'dobg-26dd9',
+	storageBucket: `dobg-26dd9.appspot.com`,
 	messagingSenderId: process.env.FB_MSG_ID,
 	appId: process.env.FB_APP_ID,
 	measurementId: process.env.FB_MEASUREMENT_ID,
